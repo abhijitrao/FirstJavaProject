1cb0cc94d8955874a80183dd3db8f00c36f9d6a0
Abhijit-PC <abhijitrao33@gmail.com>
2017-04-12 15:24:24 +0530

    update in test class

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,9 @@
 
 public class Test {
-	int i = 0;
+	static int i = 0;
 	public static void main(String Args[]){
 		
-		for (int i=05;i<10;i++){
+		for (i=05;i<10;i++){
 			System.out.println(i+" Indexing");
 		}
 	}
